07739f9c52d62fff8ec75ac3fae069434beb738c
Torisutan <scorpion.clowez77@gmail.com>
2022-09-11 15:06:40 +0200

    Add function on window load who set cookies and class data-theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as cookie from "./utils/cookie";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,5 +11,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+window.addEventListener("load", () => {
+    let lang = cookie.getCookie("lang");
+    let theme = cookie.getCookie("theme");
+    if (lang === undefined) {
+        cookie.setCookie("lang", "fr");
+    }
+    if (theme === undefined) {
+        cookie.setCookie("theme", "light");
+    }
+    document.getElementsByTagName("body")[0].setAttribute("data-theme", cookie.getCookie("theme"));
+})
+
 
 reportWebVitals();
