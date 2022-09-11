18b4843125bc598057b53201d3ab288bebec3949
Torisutan <scorpion.clowez77@gmail.com>
2022-09-11 16:42:56 +0200

    Finish toolbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,28 +1,48 @@
 import React from "react";
-import * as text from "../utils/text"
-import { getCookie} from "../utils/cookie";
+import {getLangText, getText} from "../utils/text"
+import { getCookie, setCookie} from "../utils/cookie";
+import "./Header.scss";
 import sun from "../resources/sun.svg"
 import moon from "../resources/moon.svg"
 
 export default class Header extends React.Component {
-    constructor(props) {
-        super(props)
+
+    changeTheme = () => {
+        let newTheme = getCookie("theme") === "light" ? "dark" : "light";
+        setCookie("theme", newTheme);
+        window.dispatchEvent(new Event("themeChange"))
+        document.getElementById("themeImg").src = (newTheme === "light") ? sun : moon;
     }
+
+    changeLang = event => {
+        let newLang = event.target.className
+        setCookie("lang", newLang);
+        window.location.reload()
+    }
+
 
     render() {
         return (
         <header>
-            <nav>
+            <nav className="toolbar">
                 <ul>
-                    <li><a href="#">{text.getText("profile")}</a></li>
-                    <li><a href="#">{text.getText("skills")}</a></li>
-                    <li><a href="#">{text.getText("projects")}</a></li>
-                    <li><a href="#">{text.getText("software")}</a></li>
-                    <li><a href="#">{text.getText("contact")}</a></li>
+                    <li>{getText("profile")}</li>
+                    <li>{getText("skills")}</li>
+                    <li>{getText("projects")}</li>
+                    <li>{getText("software")}</li>
+                    <li>{getText("contact")}</li>
+                    <li style={{float: "right"}} onClick={this.changeTheme}><img id="themeImg" src={(getCookie("theme") === "light") ? sun : moon}/></li>
+                    <div className="lang">
+                        <li id="langText" style={{float: "right"}}>{getText("lang")}</li>
+                        <div style={{float: "right"}} className="langList">
+                            <p className="fr" onClick={this.changeLang}>{getLangText("fr", "lang-alt")}</p>
+                            <p className="en" onClick={this.changeLang}>{getLangText("en", "lang-alt")}</p>
+                            <p className="es" onClick={this.changeLang}>{getLangText("es", "lang-alt")}</p>
+                        </div>
+                    </div>
                 </ul>
             </nav>
-            <h1>Salut 👋🏻, je suis Tristan</h1>
-            <img src={(getCookie("theme") === "light") ? sun : moon} alt=""/>
+            <h1>{getText("title")}</h1>
         </header>
         );
     }
