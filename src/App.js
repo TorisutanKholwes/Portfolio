80c8f79996b12352bdddb7813c7431d11354db4a
Torisutan <scorpion.clowez77@gmail.com>
2022-09-11 16:40:27 +0200

    Add event receptor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import Header from "./component/Header";
+import {getCookie} from "./utils/cookie";
+import "./App.scss"
 
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        window.addEventListener("themeChange", function () {
+            let newTheme = getCookie("theme");
+            document.getElementsByTagName("body")[0].setAttribute("data-theme", newTheme);
+        })
+    }
+
+
     render() {
         return (
             <Header />
