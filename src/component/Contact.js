dfc0c940c142f94f4479dee177efdcd3d08f675b
Torisutan <scorpion.clowez77@gmail.com>
2022-09-18 13:30:30 +0200

    Add Contact and style it

---
+++ src/component/Contact.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./Contact.scss";
+
+import github from "../resources/github.svg";
+import instagram from "../resources/instagram.svg"
+import twitter from "../resources/twitter.svg"
+import discord from "../resources/discord.svg"
+import mail from "../resources/mail.svg"
+
+export default class Contact extends React.Component {
+
+    openContactLink = e => {
+        window.open(e.target.getAttribute("link"), "_blank");
+    }
+
+    copyUsername() {
+        navigator.clipboard.writeText("Torisutan#0146");
+        document.getElementById("copyLink").style.display = "block";
+        setTimeout(() => {
+            document.getElementById("copyLink").style.display = "none";
+        }, 3000);
+    }
+
+    render() {
+        return (
+        <div id="contact">
+            <h1 id="contactTitle">Contact</h1>
+            <div id="contactDiv">
+                <img className="contactImage" onClick={this.copyUsername} src={discord} alt=""/>
+                <img className="contactImage" onClick={this.openContactLink} link="https://github.com/TorisutanKholwes" src={github} alt=""/>
+                <img className="contactImage" onClick={this.openContactLink} src={instagram} link="https://www.instagram.com/torisutan_clw/" alt=""/>
+                <img className="contactImage" onClick={this.openContactLink} src={twitter} link="https://twitter.com/torisutan_Khlws" alt=""/>
+                <img className="contactImage" onClick={this.openContactLink} src={mail} link="mailto:tristanclowez.contact@gmail.com" alt=""/>
+            </div>
+            <p id="copyLink" style={{display: "none"}}>Lien copié</p>
+        </div>
+        );
+    }
+}
