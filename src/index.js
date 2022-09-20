877f29b5529d986f7600b96c0886cbe926915537
Torisutan <scorpion.clowez77@gmail.com>
2022-09-20 16:06:43 +0200

    Add custom cursor and animate on click

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as cookie from "./utils/cookie";
+import "./index.scss"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -20,7 +21,17 @@ window.addEventListener("load", () => {
         cookie.setCookie("theme", "light");
     }
     document.getElementsByTagName("body")[0].setAttribute("data-theme", cookie.getCookie("theme"));
-})
 
+    const cursor = document.querySelector('#cursor');
+    window.onmousemove = (e) => {
+        cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)
+    }
+    window.onclick = (e) => {
+        cursor.classList.add('expand');
+        setTimeout(() => {
+            cursor.classList.remove('expand');
+        }, 500);
+    }
+})
 
 reportWebVitals();
