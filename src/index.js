50e9f13518388f4b7c6f6571470e9a7c67c33ea9
TheScorpion476 <scorpion.clowez77@gmail.com>
2023-01-18 11:10:35 +0100

    Reset website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as cookie from "./utils/cookie";
 import "./index.scss"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,28 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
-
-window.addEventListener("load", () => {
-    let lang = cookie.getCookie("lang");
-    let theme = cookie.getCookie("theme");
-    if (lang === undefined) {
-        cookie.setCookie("lang", "fr");
-    }
-    if (theme === undefined) {
-        cookie.setCookie("theme", "light");
-    }
-    document.getElementsByTagName("body")[0].setAttribute("data-theme", cookie.getCookie("theme"));
-
-    const cursor = document.querySelector('#cursor');
-    window.onmousemove = (e) => {
-        cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)
-    }
-    window.onclick = (e) => {
-        cursor.classList.add('expand');
-        setTimeout(() => {
-            cursor.classList.remove('expand');
-        }, 500);
-    }
-})
 
 reportWebVitals();
